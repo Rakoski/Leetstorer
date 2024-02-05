e01559af3294f002d4b13b8a7302f38b45f0aec5
Rakoski <mastrakoski@gmail.com>
2024-02-05 20:10:51 -0300

    feat(Article/index.tsx): Create reusable ArticleComponent
    - Added a new reusable ArticleComponent for displaying forms with a title, input fields, and a submit button.
    - Centered the component horizontally and positioned it slightly below the center vertically.
    - Adjusted input field width for a cleaner and more spacious appearance.

---
+++ my-turborepo/apps/admin/src/app/Login/index.tsx
@@ -0,0 +1,21 @@
+import {useState} from "react";
+import {InputField} from "@repo/ui/src/InputField";
+import {ArticleComponent} from "@repo/ui/src/Article";
+
+function LoginPage(): JSX.Element {
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+
+    const handleLogin = () => {
+        console.log("User registered:", { email });
+    };
+
+    const loginFields = [
+        <InputField key="email" label={"Email"} type="email" value={email} onChange={(e) => setEmail(e.target.value)} />,
+        <InputField key="password" label={"Password"} type="password" value={password} onChange={(e) => setPassword(e.target.value)} />,
+    ];
+
+    return <ArticleComponent title="Sign in on LeetStorer" fields={loginFields} onSubmit={handleLogin} />;
+}
+
+export default LoginPage;
--- my-turborepo/apps/admin/src/main.tsx
+++ my-turborepo/apps/admin/src/main.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./app";
+import App from "./app/Registration";
 import "./index.css";
 
 const el = document.getElementById("root");
