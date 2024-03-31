fc554fcc755379c5c0f98a1d4261de70e161ef75
Rakoski <mastrakoski@gmail.com>
2024-03-31 16:52:20 -0300

    feat: (existing_problems): Created new existingProblemsResolver.ts
    - Added new existing_problems.ts resolver
    - Updated graphql.ts by adding it as a resolver as well

--- my-turborepo/apps/api/src/graphql/schema/index.ts
+++ my-turborepo/apps/api/src/graphql/schema/index.ts
@@ -66,7 +66,7 @@ module.exports = buildSchema(`
   type RootQuery {
     problems: [Problem!]!
     users: [User!]!
-    existingProblems: [ExistingProblems!]!
+    existing_problems: [ExistingProblems!]!
   }
 
   type RootMutation {
