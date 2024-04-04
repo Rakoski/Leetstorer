5ae320cd99cc3a6336ce0594b7ce1c95ef11070c
Rakoski <mastrakoski@gmail.com>
2024-04-04 13:17:28 -0300

    fix (DescriptionField/index.tsx) Make description field bigger
    - still have to correct columns and create a spearate row field

--- my-turborepo/packages/ui/src/DescriptionField/index.tsx
+++ my-turborepo/packages/ui/src/DescriptionField/index.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const DescriptionField = styled.textarea`
     width: 100%;
-    height: 700px;
+    height: 701px;
     padding: 0.8rem;
     font-size: 1.1rem;
     border: 1px solid #ccc;
