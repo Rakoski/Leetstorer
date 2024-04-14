ebb8167ba4a2ad6f2489b39bdb3c25e9c8d0b7b1
Rakoski <mastrakoski@gmail.com>
2024-04-14 17:18:32 -0300

    feat: (InfoField/index.tsx): Added new link property to InfoField

--- my-turborepo/packages/ui/src/InfoField/index.tsx
+++ my-turborepo/packages/ui/src/InfoField/index.tsx
@@ -1,6 +1,21 @@
 import styled from 'styled-components';
+import React, { HTMLAttributes } from 'react';
 
-const InfoField = styled.input`
+interface InfoFieldProps extends HTMLAttributes<HTMLInputElement> {
+    value: string;
+    link?: boolean;
+    readOnly?: boolean;
+}
+
+const InfoField: React.FC<InfoFieldProps> = ({ value, link, readOnly, ...rest }) => {
+    if (link && value && /^https?:\/\//.test(value)) {
+        return <a href={value} {...rest}>{value}</a>;
+    }
+
+    return <StyledInput value={value} readOnly={readOnly} />;
+};
+
+const StyledInput = styled.input`
   width: 100%;
   padding: 0.8rem;
   font-size: 1.1rem;
