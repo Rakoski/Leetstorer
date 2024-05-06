505afacbf4e35c3937020be9c5535c5166c3af34
Rakoski <mastrakoski@gmail.com>
2024-05-06 07:20:19 -0300

    fix: tests
    - Fixed testing base
    - Commiting because I want to open another branch to fix my code and run all the tests

--- my-turborepo/apps/api/src/graphql/resolvers/userResolver.ts
+++ my-turborepo/apps/api/src/graphql/resolvers/userResolver.ts
@@ -1,30 +1,32 @@
 import bcrypt from "bcryptjs";
-import {log} from "@repo/logger";
-import {ProblemInterface} from "./utils/problemInterface.ts";
+import { log } from "@repo/logger";
 import jwt from "jsonwebtoken";
+import {UserInterface} from "./utils/userInterface.ts";
+import {ProblemInterface} from "./utils/problemInterface.ts";
+import {Document, Types} from "mongoose";
+const User = require('../../models/user.ts')
+const Problem = require('../../models/problem.ts')
 
-const Problem = require('../../models/problem');
-const User = require('../../models/user.ts');
-
-const userCreator = require('./utils/userCreator.ts')
+const userCreator = require('./utils/userCreator');
 
 module.exports = {
-    users: async (args: object, req: {isAuth: boolean}) => {
+    users: async (_args: unknown, req: { isAuth: boolean }) => {
         if (!req.isAuth) {
-            throw new Error("Unauthorized!")
+            throw new Error("Unauthorized!");
         }
 
         try {
-            let users = null
-            users = await User.find().populate('createdProblems');
+            let users = UserInterface;
+             users = await User.find().populate('createdProblems');
 
-            return users.map((user: { _doc: { _id: string }, _id: string, username: string, email: string,
-                createdProblems: Array<ProblemInterface>}) => ({
-                ...user,
+            if (!users) {
+                return new Error("Could not retrieve user and it's problems");
+            }
+
+            return users.map((user) => ({
+                ...user._doc,
                 _id: user._id.toString(),
-                email: user.email,
-                username: user.username,
-                createdProblems: user.createdProblems.map((problem) => ({
+                createdProblems: user.createdProblems.map((problem: ProblemInterface) => ({
                     title: problem.title,
                     level: problem.level,
                     description: problem.description,
@@ -41,38 +43,39 @@ module.exports = {
             throw err;
         }
     },
-    createUser: async (args: {userInput: { username: string, email: string; password: string }}) => {
+    createUser: async (_args: unknown, { userInput: { username, email, password } }: { userInput: { username: string, email: string, password: string } }) => {
         try {
-            const existingUser = await User.findOne({ email: args.userInput.email });
+            const existingUser = await User.findOne({ email });
 
             if (existingUser) {
                 return new Error("User already exists!");
             }
 
-            const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
+            const hashedPassword = await bcrypt.hash(password, 12);
 
             const user = new User({
-                username: args.userInput.username,
-                email: args.userInput.email,
+                username,
+                email,
                 password: hashedPassword,
             });
 
-            const result = await user.save();
+            let result = null
+            result = await user.save();
 
-            return { ...result, password: null, _id: result._id, email: result.email, username: result.username };
-
+            if (result) {
+                return { ...result._doc, password: null };
+            }
         } catch (err) {
             log("Error in createUser resolver: ", err);
             throw new Error("Error in creating user");
         }
     },
-    login: async ({ email, password }) => {
+    login: async (_args: unknown, { email, password }: { email: string, password: string }) => {
         try {
             let user = null
-
             user = await User.findOne({ email });
             if (!user) {
-                return new Error("User does not exist!");
+                return new Error("404 " + email +  " User not found!");
             }
 
             const passwordsAreEqual = await bcrypt.compare(password, user.password);
@@ -80,11 +83,15 @@ module.exports = {
                 return new Error("401 Invalid credentials");
             }
 
-            const token = jwt.sign({ userId: user.id, email: user.email }, process.env.JWT_KEY, { expiresIn: '48h' });
+            const token = process.env.JWT_KEY
+                ? jwt.sign({ userId: user.id, email: user.email }, process.env.JWT_KEY, { expiresIn: '48h' })
+                : null;
 
             return { userId: user.id, token, tokenExpiration: 48 };
         } catch (err) {
-            console.error("Error in login:", err.message);
+            if (err instanceof Error) {
+                console.error("Error in login:", err.message);
+            }
             throw err;
         }
     },
@@ -92,13 +99,13 @@ module.exports = {
     // because I configured my server without the proper relay modifications, I will need to do some concessions,
     // mainly, I basically cannot do queries since I don't have the Node type. I will then, use mutations to fetch
     // my users problems.
-    getUserProblems: async (args: { userId: string }, req: {isAuth: boolean, isAdmin: boolean}) => {
+    getUserProblems: async (_args: unknown, args: { userId: string }, req: { isAuth: boolean, isAdmin: boolean }) => {
         if (!req.isAuth || !req.isAdmin) {
             throw new Error("Unauthorized!");
         }
 
         try {
-            let user = new User()
+            let user = null
 
             user = await User.findById(args.userId).populate('createdProblems');
 
@@ -106,7 +113,7 @@ module.exports = {
                 return new Error("User not found!");
             }
 
-            return user.createdProblems.map((problem: object) => ({
+            return user.createdProblems.map((problem) => ({
                 _id: problem._id.toString(),
                 title: problem.title,
                 level: problem.level,
@@ -115,19 +122,18 @@ module.exports = {
                 frequency: problem.frequency,
                 link: problem.link,
                 data_structure: problem.data_structure,
-                date: problem.date.toString()
+                date: problem.date?.toString()
             }));
         } catch (err) {
             console.log("Error in getUserProblems resolver: ", err);
             throw err;
         }
     },
-    associateUserWithProblem: async (args: { userId: string, problemId: string }) => {
+    associateUserWithProblem: async (_args: unknown, args: { userId: string, problemId: string }) => {
         try {
             const { userId, problemId } = args;
 
-            let user = null
-            let problem = new Problem()
+            let user, problem = null
 
             user = await User.findById(userId);
             problem = await Problem.findById(problemId);
@@ -148,7 +154,7 @@ module.exports = {
                 frequency: problem.frequency,
                 link: problem.link,
                 data_structure: problem.data_structure,
-                date: problem.date.toString(),
+                date: problem.date?.toString(),
                 creator: await userCreator(userId),
             };
         } catch (err) {
--- my-turborepo/apps/api/src/graphql/resolvers/utils/userInterface.ts
+++ my-turborepo/apps/api/src/graphql/resolvers/utils/userInterface.ts
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 import {Document} from "mongodb";
+import {ProblemInterface} from "./problemInterface.ts";
 
 export interface UserInterface extends Document {
     _id: mongoose.Types.ObjectId[];
     username: string;
     email: string;
-    createdProblems: mongoose.Types.ObjectId[] | string;
+    isAdmin: boolean
+    createdProblems: ProblemInterface[];
 }
